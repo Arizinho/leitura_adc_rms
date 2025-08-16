a54ce6068dab32741f16fcd8d523539e0026e0c3
Ariagildo L <ariagildo01@gmail.com>
2025-08-16 02:34:27 -0300

    commit inicial

--- adc_reader.c
+++ adc_reader.c
@@ -2,16 +2,22 @@
 #include <math.h>
 #include "pico/stdlib.h"     // Biblioteca padrão do Raspberry Pi Pico
 #include "hardware/adc.h"
-#include "hardware/uart.h"
 #include "hardware/gpio.h"
 
-#define UART_ID uart0
-#define BAUD_RATE 115200
-
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
 #define ADC_VOLT 27 // GPIO para o voltímetro
 #define ADC_AMP 26 // GPIO para o amperímetro
+
+#define AMOSTRAS 256
+#define ATRASO 10 
+
+const float WC = 2.0f * M_PI * 106.0f;
+const float A  = 2.0f * 7692.0f;
+const float B  = WC * sqrtf(2.0f);
+const float C  = WC * WC;
+
+const float X1 = C / (A*A + A*B + C);
+const float X2 = 2.0f * (C - A*A) / (A*A + A*B + C);
+const float X3 = (A*A + C - A*B) / (A*A + A*B + C);
 
 int main()
 {
@@ -22,49 +28,98 @@ int main()
     adc_gpio_init(ADC_VOLT); // GPIO 27 como entrada analógica
     adc_gpio_init(ADC_AMP);  // GPIO 26 como entrada analógica
 
-    //Inicializa comunicação UART
-    uart_init(UART_ID, BAUD_RATE);
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-
     float tensao, corrente;
 
-    uint16_t leitura_tensao, leitura_corrente;
-
-    char buffer_uart[64];
+    float leitura_tensao[AMOSTRAS], leitura_corrente[AMOSTRAS];
+    float filtro_tensao[AMOSTRAS/2], filtro_corrente[AMOSTRAS/2];
 
     while (true)
     {  
         tensao = 0.0;
         corrente = 0.0;
 
-        //Calcula valor RMS a partir de 4000 leituras
-        for (int i = 0; i < 4000; i++) {
-            adc_select_input(1); // Tensão
-            leitura_tensao = adc_read();
+        for (int i = 0; i < AMOSTRAS; i++) {
+            adc_select_input(1); // Corrente
+            leitura_tensao [i] = ((float)adc_read() - 1585.0)*0.6617;
 
-            adc_select_input(0); // Corrente
-            leitura_corrente = adc_read();
+            if(i>=ATRASO) {
+                adc_select_input(0); // Corrente
+                leitura_corrente[i-ATRASO] = (-(float)adc_read() + 1585.0)*0.021055;
+            }
 
-            float tensao_ac = (float)leitura_tensao - 1585.0;
-            float corrente_ac = (float)leitura_corrente - 1585.0;
+            
 
-            tensao += tensao_ac * tensao_ac;
-            corrente += corrente_ac * corrente_ac;
-            sleep_us(50);
+            // float tensao_ac = (float)leitura_tensao - 1585.0;
+            // float corrente_ac = (float)leitura_corrente - 1585.0;
+
+            // tensao += tensao_ac * tensao_ac;
+            // corrente += corrente_ac * corrente_ac;
+            sleep_us(130);
         }
 
-        // Calibação dos valores RMS
-        tensao = sqrt(tensao / 4000.0) * 0.618;
-        corrente = sqrt(corrente / 4000.0) * 0.024338;
+        adc_select_input(0); // Corrente
+        for (int i = AMOSTRAS; i < AMOSTRAS+ATRASO; i++) {
+            leitura_corrente[i-ATRASO] = (-(float)adc_read() + 1585.0)*0.021055;
+        }
 
-        // Formata e envia os dados via UART
-        snprintf(buffer_uart, sizeof(buffer_uart), "T%.2f C%.2f \n", tensao, corrente);
-        uart_puts(UART_ID, buffer_uart);
 
-        printf(buffer_uart);
+        filtro_tensao [0] = 0.0;
+        filtro_tensao [1] = 0.0;
+        filtro_corrente [0] = 0.0;
+        filtro_corrente [1] = 0.0;
 
-        sleep_ms(500);
+        float y_1 = 0.0, y_2 = 0.0;  // estados para tensão
+        float z_1 = 0.0, z_2 = 0.0;  // estados para corrente
+
+        for (int i = 0; i < AMOSTRAS; i++) {
+            float y, z;
+
+            if (i < 2) {
+                y = 0.0;
+                z = 0.0;
+            } else {
+                // Filtro tensão
+                y = -X2*y_1 - X3*y_2
+                    + X1*leitura_tensao[i]
+                    + 2*X1*leitura_tensao[i-1]
+                    + X1*leitura_tensao[i-2];
+
+                // Filtro corrente
+                z = -X2*z_1 - X3*z_2
+                    + X1*leitura_corrente[i]
+                    + 2*X1*leitura_corrente[i-1]
+                    + X1*leitura_corrente[i-2];
+            }
+
+            // Só grava a segunda metade
+            if (i >= AMOSTRAS/2) {
+                filtro_tensao[i - AMOSTRAS/2] = y;
+                filtro_corrente[i - AMOSTRAS/2] = z;
+            }
+
+            // Atualiza estados
+            y_2 = y_1;  y_1 = y;
+            z_2 = z_1;  z_1 = z;
+        }
+
+        double soma_v = 0.0, soma_i = 0.0, soma_p = 0.0;
+
+        for(int i = 0; i < AMOSTRAS/2; i++){
+            soma_v += filtro_tensao[i] * filtro_tensao[i];
+            soma_i += filtro_corrente[i] * filtro_corrente[i];
+            soma_p += filtro_tensao[i] * filtro_corrente[i];
+        }
+
+        float V_rms = sqrtf(soma_v / (AMOSTRAS/2));
+        float I_rms = sqrtf(soma_i / (AMOSTRAS/2));
+        float P_media = soma_p / (AMOSTRAS/2);
+        float P_aparente = I_rms*V_rms;
+        float fp = P_media/P_aparente;
+
+        printf("\n\nTensao RMS: %.2fV\nCorrente RMS: %.2fA\nPotencia Real: %.2fW\nPotencia aparente: %.2fVA\nFator de Potencia: %.2f",
+                V_rms, I_rms, P_media, P_aparente, fp);
+
+        sleep_ms(4000);
 
     }
 }
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_reader
+# Install script for directory: C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_test
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/mqtt-client")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/leitor_adc")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -39,14 +39,14 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_reader/build/pico-sdk/cmake_install.cmake")
+  include("C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_test/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_reader/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_test/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -62,6 +62,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_reader/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/ari_z/Desktop/Embarcatech/PROJETO LESTART/adc_test/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
